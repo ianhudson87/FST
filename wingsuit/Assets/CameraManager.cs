520f877a84f23c0477e7043adc34b24a7d0dc8ea
ianhudson87 <ihudson@wustl.edu>
2022-07-11 16:14:10 -0500

    camera zoom at high speed

---
+++ wingsuit/Assets/CameraManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] float camMinSpeed, camMaxSpeed, camDistMultiplier;
+    [SerializeField] PlayerController pc;
+    Vector3 cameraDefaultPos;
+
+    void Awake() {
+        cameraDefaultPos = transform.localPosition;
+    }
+
+    void Update() {
+        float speedPercentage = Mathf.Clamp((pc.GetVelocity().magnitude - camMinSpeed) / (camMaxSpeed-camMinSpeed), 0, 1);
+        transform.localPosition = cameraDefaultPos * (speedPercentage * (camDistMultiplier - 1) + 1);
+        Debug.Log("precentage" + speedPercentage);
+    }
+
+}
